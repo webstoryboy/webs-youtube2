36b400d066d6ee4938d13685b90e789904cc69c7
웹스토리보이 <34331694+websfont@users.noreply.github.com>
2023-10-12 17:44:19 +0900

    추천 개발자 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import { developerText } from '../../data/developer';
+import { Link } from 'react-router-dom';
+
 const Developer = () => {
     return (
-        <div>Developer</div>
+        <section id='developer'>
+            <h2>😪 추천 개발자를 소개합니다.</h2>
+            <div className="develpoer__inner overflow">
+                {developerText.map((developer, key) => (
+                    <div className="develpoer" key={key}>
+                        <div className="develpoer__img play__icon">
+                            <Link to={`/channel/${developer.channelId}`}>
+                                <img src={developer.img} alt={developer.name} />
+                            </Link>
+                        </div>
+                        <div className="develpoer__info">
+                            <Link to={`/channel/${developer.channelId}`}>
+                                {developer.name}
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
 
