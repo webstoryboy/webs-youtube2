4276736e9f79ddf6087ebab4d78198d1662c6bd3
웹스토리보이 <34331694+websfont@users.noreply.github.com>
2023-10-12 13:46:48 +0900

    페이지 SEO 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Channel = () => {
     return (
-		<main>
-			Channel
-		</main>
+        <Main 
+            title = "유튜브 채널"
+            description="유튜브 채널페이지입니다.">
+            Channel
+        </Main>
     )
 }
 
