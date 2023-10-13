787cb5eccd018734f0b673bffff864d558863e1e
웹스토리보이 <34331694+websfont@users.noreply.github.com>
2023-10-14 08:40:37 +0900

    검색 및 유튜브 API 설정하기

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        console.log(searchKeyword)
+        if (searchKeyword) {
+            navigate(`/search/${searchKeyword}`);
+            setSearchKeyword('');
+        }
+    };
+
     return (
         <div id='search'>
             <div className='search__inner'>
@@ -13,9 +25,16 @@ const Search = () => {
                     placeholder='검색어를 입력해주세요' 
                     autoComplete='off' 
                     className='search__input' 
+                    onChange={e => setSearchKeyword(e.target.value)}
+                    onKeyDown={e => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
                 />
             </div>
         </div>
     )
 }
+
 export default Search
